c9e223446b62266175551843f92ccb49902e0a6a
megadev <contato.wanjalagus@outlook.com.br>
2023-01-21 16:24:44 -0400

    refactoring: inserido comentarios sobre cada metodo para melhor leitura

--- app/Repositories/Installment/InstallmentRepository.php
+++ app/Repositories/Installment/InstallmentRepository.php
@@ -6,8 +6,21 @@ use App\Models\Installment;
 //TODO Documentar o código
 class InstallmentRepository {
     /**
-     * Essa função avalia o nome do usuário e retorna:
-     * Se a
+     * Essa função avalia o nome do usuário e retorna o a.k.a "apelido":
+     *
+     * Se o nome inserido tiver mais de 1 palavra, ou seja, nome completo,
+     * retorna a primeira letra do nome e a primeira letra do último nome,
+     * sempre maiusculo, por exemplo:
+     *
+     * Gustavo de Camargo Campos -> GC
+     * Aline da Silva Santos -> AS
+     *
+     * Se o nome inserido for apenas o primeiro nome, vai retornar a primeira e
+     * terceira letra do nome sempre em maiusculo, exemplo:
+     *
+     * Gustavo -> GS
+     * Aline -> AI
+     *
      */
     public static function generateAkaNameUser($arrayInstallments) {
         $result = array_map(function($i) {
@@ -19,7 +32,14 @@ class InstallmentRepository {
         }, $arrayInstallments);
         return $result;
     }
-    public static function trataFiltros($filtros) {
+    /**
+     * A função retorna um array com todos os filtros recebidos como parametro
+     *
+     * É feito a tratativa se tem mais de um grupo de filtro, caso, tenha, o
+     * explode é usado para separar os tipos de filtros separados por ; e novamente
+     * separando tabela, operador e valor com :
+     */
+    public static function handleFilters($filtros) {
         $filtrosTratados = [];
         $filtros = explode(';', $filtros);
         foreach($filtros as $filtro) {
@@ -27,6 +47,10 @@ class InstallmentRepository {
         }
         return $filtrosTratados;
     }
+
+    /**
+     * Essa funçãoo determina se o titulo está vencido ou não
+     */
     public static function determineIfInstallmentsAreOverduePayment($arrayInstallments) {
         return array_map(function ($installment) {
             if($installment['due_date'] < date('Y-m-d')) {
@@ -36,14 +60,21 @@ class InstallmentRepository {
             return $installment;
         }, $arrayInstallments);
     }
+
+    /**
+     * Faz o retorno do titulo quando o mesmo não contem filtros
+     */
     public static function getTheInstallmentsWhenThereIsNoFilter() {
         $installments = Installment::with('user:id,name')
         ->select('id', 'users_id', 'id_billing','status', 'debtor', 'emission_date', 'due_date', 'overdue_payment' ,'amount', 'paid_amount')
         ->get();
         return InstallmentRepository::generateAkaNameUser(InstallmentRepository::determineIfInstallmentsAreOverduePayment($installments->toArray()));
     }
+    /**
+     * Faz o retorno do titulo quando é utilizado filtros na consulta
+     */
     public static function getTheInstallmentsWhenThereFilter($filtros) {
-        $filtrosTratados = InstallmentRepository::trataFiltros($filtros);
+        $filtrosTratados = InstallmentRepository::handleFilters($filtros);
         $allInstallments = Installment::with('user:id,name');
         $allInstallments
         ->select('id', 'users_id', 'id_billing','status', 'debtor', 'emission_date', 'due_date', 'overdue_payment' ,'amount', 'paid_amount')
@@ -53,6 +84,9 @@ class InstallmentRepository {
         }
         return $allInstallments->get()->toArray();
     }
+    /**
+     * É a função que é usada para retornar ao Controller todo procedimento formatado.
+     */
     public function push($filtros = null) {
         if(is_null($filtros)) {
             return InstallmentRepository::getTheInstallmentsWhenThereIsNoFilter();
